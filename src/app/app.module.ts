23b98ad96e307f34c7d175196792a46e12ce50f7
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-04 16:20:09 -0700

    Components declarated and APP_ROUTES imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, PhotosComponent, UploadComponent],
+  imports: [BrowserModule, APP_ROUTES],
   providers: [],
   bootstrap: [AppComponent],
 })

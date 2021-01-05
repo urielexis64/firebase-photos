17ce385259bf4b4af87732cd2716e377cfa83d6b
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-04 22:13:28 -0700

    loadImages

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FileItem } from '../../models/file-item';
+import { UploadImagesService } from '../../services/upload-images.service';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
+  files: FileItem[] = [];
 
-  constructor() { }
+  constructor(public _uploadService: UploadImagesService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  loadImages() {
+    this._uploadService.loadFirebaseImages(this.files);
   }
-
 }

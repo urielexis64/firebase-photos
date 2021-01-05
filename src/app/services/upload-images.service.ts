7f6900eb18667d69bba70f69f63cfeebc3c1dfc8
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-04 22:17:14 -0700

    load images from Firebase

--- src/app/services/upload-images.service.ts
+++ src/app/services/upload-images.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import * as firebase from 'firebase';
+import { FileItem } from '../models/file-item';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +10,9 @@ export class UploadImagesService {
   private IMAGES_FOLDER = 'img';
 
   constructor(private db: AngularFirestore) {}
+  loadFirebaseImages(images: FileItem[]) {
+    console.log(images);
+  }
   private saveImage(image: any) {
     this.db.collection(`/${this.IMAGES_FOLDER}`).add(image);
   }

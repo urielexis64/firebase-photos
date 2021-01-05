910617e003609759183ac1e54ab0aef8bfab4de2
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-04 19:59:13 -0700

    firebase files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDcdMqwLlZ7LsZ2zjTcsi5SVX5A_0gktP0',
+    authDomain: 'fir-photos-3b9c9.firebaseapp.com',
+    projectId: 'fir-photos-3b9c9',
+    storageBucket: 'fir-photos-3b9c9.appspot.com',
+    messagingSenderId: '38943514457',
+    appId: '1:38943514457:web:0ad3aee401cf0c181bf648',
+    measurementId: 'G-ZZE6N203Q2',
+  },
 };
 
 /*

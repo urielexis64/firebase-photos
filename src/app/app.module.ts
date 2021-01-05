1fc14c13ca9d02edf26252eaacb7f5006bf4140a
Uriel Aispuro <urielalexis64@gmail.com>
2021-01-04 19:59:29 -0700

    init AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './components/photos/photos.component';
 import { UploadComponent } from './components/upload/upload.component';
 import { APP_ROUTES } from './app.routes';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, PhotosComponent, UploadComponent],
-  imports: [BrowserModule, APP_ROUTES],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
